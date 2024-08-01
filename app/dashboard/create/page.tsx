990ffffcdf0740dc51c8a0b098a171a7dd45d407
nikilok <nikilok@gmail.com>
2024-08-01 17:16:41 +0100

    remove the restriction of having to have a supplier to render the create form page

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/app/components/ui/button";
 import {
 	Card,
 	CardDescription,
@@ -8,7 +7,6 @@ import {
 import { Separator } from "@/app/components/ui/separator";
 import { getSuppliers } from "@/app/lib/data";
 import Form from "@/app/ui/contracts/create/create-contracts";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Page() {
@@ -16,29 +14,18 @@ export default async function Page() {
 
 	return (
 		<main className="py-8 px-2">
-			{suppliers.length > 0 ? (
-				<Card className="w-full max-w-4xl m-auto">
-					<CardHeader>
-						<CardTitle>Create Contract</CardTitle>
-						<CardDescription>
-							Please fill out the form to submit a new contract request.
-						</CardDescription>
-					</CardHeader>
-					<Separator className="mb-4" />
-					<Suspense fallback="loading...">
-						<Form suppliers={suppliers} />
-					</Suspense>
-				</Card>
-			) : (
-				<Card className="p-10 flex flex-col items-center w-full max-w-4xl m-auto">
-					<div>
-						<sup>No suppliers found</sup>
-					</div>
-					<Link href="/dashboard/suppliers">
-						<Button variant="outline">Add suppliers first</Button>
-					</Link>
-				</Card>
-			)}
+			<Card className="w-full max-w-4xl m-auto">
+				<CardHeader>
+					<CardTitle>Create Contract</CardTitle>
+					<CardDescription>
+						Please fill out the form to submit a new contract request.
+					</CardDescription>
+				</CardHeader>
+				<Separator className="mb-4" />
+				<Suspense fallback="loading...">
+					<Form suppliers={suppliers} />
+				</Suspense>
+			</Card>
 		</main>
 	);
 }
