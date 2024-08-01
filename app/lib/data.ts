4727a77084c02b095f2e6a4353f56b8e89df4113
nikilok <nikilok@gmail.com>
2024-08-01 22:25:50 +0100

    correct error logs

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,7 @@ export async function getSuppliers() {
 
 		return data.map((row) => ({ value: row.name, label: row.name }));
 	} catch (err) {
-		throw new Error("Database error: failed to get invoice pending");
+		throw new Error("Database error: failed to get suppliers");
 	}
 }
 
@@ -27,6 +27,6 @@ export async function addSupplier(name: string) {
 
 		revalidatePath("/dashboard/create");
 	} catch (err) {
-		throw new Error("Database error: failed to get invoice pending");
+		throw new Error("Database error: failed to add supplier");
 	}
 }
