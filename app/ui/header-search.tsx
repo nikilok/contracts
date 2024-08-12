961bec47377d9c74a259d5e35ce6e2316afe4566
nikilok <nikilok@gmail.com>
2024-08-12 14:18:54 +0100

    add search functionality

---
+++ app/ui/header-search.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { Input } from "../components/ui/input";
+
+export default function HeaderSearch({ placeholder }: { placeholder: string }) {
+	const searchParams = useSearchParams();
+	const pathName = usePathname();
+	const { replace } = useRouter();
+
+	const handleSearch = useDebouncedCallback((term: string) => {
+		const params = new URLSearchParams(searchParams);
+
+		if (term) {
+			params.set("page", "1");
+			params.set("query", term);
+		} else {
+			params.delete("query");
+		}
+
+		replace(`/dashboard/contracts?${params.toString()}`);
+	}, 500);
+
+	return (
+		<form
+			action={(e) => {
+				console.log("submitted");
+			}}
+			className="ml-auto flex-1 sm:flex-initial"
+		>
+			<div className="relative">
+				<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+				<Input
+					type="search"
+					className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+					placeholder={placeholder}
+					onChange={(e) => handleSearch(e.target.value)}
+					defaultValue={searchParams.get("query")?.toString()}
+				/>
+			</div>
+		</form>
+	);
+}
