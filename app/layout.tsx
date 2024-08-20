b3936dcd2f281a4a415b931aa17d463651927208
nikilok <nikilok@gmail.com>
2024-08-20 19:56:21 +0100

    move the head to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,17 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-			<SpeedInsights />
-		</html>
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, maximum-scale=1"
+				/>
+			</Head>
+			<html lang="en">
+				<body className={inter.className}>{children}</body>
+				<SpeedInsights />
+			</html>
+		</>
 	);
 }
