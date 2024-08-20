b58e451bdd07643f5d52153e0963d85dda1f8805
nikilok <nikilok@gmail.com>
2024-08-20 20:14:11 +0100

    testing adding a meta tag in the next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  head: {
+    // Add your custom meta tag
+    meta: [
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1, maximum-scale=1",
+      },
+      // Other meta tags if needed
+    ],
+  },
+};
 
 export default nextConfig;
