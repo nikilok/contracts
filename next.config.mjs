923eccbd2c84515af9dbd79dad973bef1717b806
nikilok <nikilok@gmail.com>
2024-08-27 18:51:10 +0100

    add viewport configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  head: {
-    // Add your custom meta tag
-    meta: [
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1, maximum-scale=1",
-      },
-      // Other meta tags if needed
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
