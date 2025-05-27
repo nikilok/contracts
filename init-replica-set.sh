ab3d32613bc2c4ac1742c4cca0b8da8b2d0ea698
nikilok <nikilok@gmail.com>
2025-05-27 23:56:38 +0100

    create variables for start timer.

--- init-replica-set.sh
+++ init-replica-set.sh
@@ -17,17 +17,17 @@ var config = {
     "members": [
         {
             "_id": 1,
-            "host": "mongo1:27017", // Reverted to internal Docker hostname
+            "host": "mongo1:27017", 
             "priority": 3
         },
         {
             "_id": 2,
-            "host": "mongo2:27017", // Reverted to internal Docker hostname
+            "host": "mongo2:27017",
             "priority": 2
         },
         {
             "_id": 3,
-            "host": "mongo3:27017", // Reverted to internal Docker hostname
+            "host": "mongo3:27017",
             "priority": 1
         }
     ]
